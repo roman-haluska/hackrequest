0f480a7bea8c8af3c8d4112e92f0467a62b0867f
Branislav Brincko <branislav.brincko@goodrequest.com>
2025-02-07 11:33:05 +0100

    Add event name to admin dashboard

--- src/app/admin/AdminDashboard.tsx
+++ src/app/admin/AdminDashboard.tsx
@@ -7,111 +7,133 @@ import { Attendee } from '@/db/schema'
 import jsPDF from 'jspdf'
 import 'jspdf-autotable'
 import { format } from 'date-fns'
+import { AttendesWithEvent } from './users-fetch'
 
 type Props = {
- users: Attendee[]
+    users: AttendesWithEvent
 }
 
 export const AdminDashboard = (props: Props) => {
- const { users } = props
- const [searchQuery, setSearchQuery] = useState('')
- const [sortConfig, setSortConfig] = useState({ key: 'id', direction: 'ascending' })
+    const { users } = props
+    const [searchQuery, setSearchQuery] = useState('')
+    const [sortConfig, setSortConfig] = useState({ key: 'id', direction: 'ascending' })
 
- const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setSearchQuery(event.target.value)
- }
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value)
+    }
 
- const handleSort = (key: keyof Attendee) => {
-  let direction = 'ascending'
-  if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-   direction = 'descending'
-  }
-  setSortConfig({ key, direction })
- }
+    const handleSort = (key: keyof Attendee) => {
+        let direction = 'ascending'
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending'
+        }
+        setSortConfig({ key, direction })
+    }
 
- const sortedUsers = [...users].sort((a, b) => {
-  if (a[sortConfig.key] < b[sortConfig.key]) {
-   return sortConfig.direction === 'ascending' ? -1 : 1
-  }
-  if (a[sortConfig.key] > b[sortConfig.key]) {
-   return sortConfig.direction === 'ascending' ? 1 : -1
-  }
-  return 0
- })
+    const sortedUsers = [...users].sort((a, b) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1
+        }
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1
+        }
+        return 0
+    })
 
- const filteredUsers = sortedUsers.filter((user) =>
-  user.fullName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
-  user.email?.toLowerCase().includes(searchQuery.toLowerCase())
- )
+    const filteredUsers = sortedUsers.filter(
+        (user) =>
+            user.eventName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.fullName?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.email?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.city?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.category?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.club?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.dateOfBirth?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.gender?.toLowerCase().includes(searchQuery.toLowerCase())
+    )
 
- const exportToPDF = () => {
-  const doc = new jsPDF()
-  doc.autoTable({
-   head: [['ID', 'Name', 'Email', 'City', 'Category', 'Club', 'Date of Birth', 'Gender']],
-   body: filteredUsers.map(user => [
-    user.id,
-    user.fullName,
-    user.email,
-    user.city,
-    user.category,
-    user.club,
-    format(new Date(user.dateOfBirth!), 'dd.MM.yyyy'),
-    user.gender
-   ])
-  })
-  doc.save('users.pdf')
- }
+    const exportToPDF = () => {
+        const doc = new jsPDF()
+        doc.autoTable({
+            head: [['ID', 'Name', 'Email', 'City', 'Category', 'Club', 'Date of Birth', 'Gender']],
+            body: filteredUsers.map((user) => [
+                user.id,
+                user.fullName,
+                user.email,
+                user.city,
+                user.category,
+                user.club,
+                format(new Date(user.dateOfBirth!), 'dd.MM.yyyy'),
+                user.gender,
+            ]),
+        })
+        doc.save('users.pdf')
+    }
 
- return (
-  <div className="container mx-auto p-6 min-h-screen ">
-   <div className="flex justify-between items-center mb-6">
-    <h1 className="text-3xl font-bold text-gray-800">Zoznam registrovaných na podujatie</h1>
-    <LogoutButton />
-   </div>
-   <div className="mb-4 flex space-x-4">
-    <input
-     type="text"
-     placeholder="Search..."
-     value={searchQuery}
-     onChange={handleSearchChange}
-     className="p-2 border border-gray-300 rounded flex-grow"
-    />
-    <button
-     onClick={exportToPDF}
-     className="p-2 bg-[#ff9a00] text-white rounded"
-    >
-     Export to PDF
-    </button>
-   </div>
-   <Table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-    <TableHeader className="bg-[#ff9a00] text-white">
-     <TableRow>
-      {['id', 'fullName', 'email', 'city', 'category', 'club', 'dateOfBirth', 'gender'].map((key) => (
-       <TableCell
-        key={key}
-        onClick={() => handleSort(key as keyof Attendee)}
-        className="cursor-pointer p-4 font-semibold"
-       >
-        {key.charAt(0).toUpperCase() + key.slice(1)}
-       </TableCell>
-      ))}
-     </TableRow>
-    </TableHeader>
-    <TableBody>
-     {filteredUsers.map((user) => (
-      <TableRow key={user.id} className="hover:bg-gray-100">
-       <TableCell className="p-4 border-b">{user.id}</TableCell>
-       <TableCell className="p-4 border-b">{user.fullName}</TableCell>
-       <TableCell className="p-4 border-b">{user.email}</TableCell>
-       <TableCell className="p-4 border-b">{user.city}</TableCell>
-       <TableCell className="p-4 border-b">{user.category || '-'}</TableCell>
-       <TableCell className="p-4 border-b">{user.club || '-'}</TableCell>
-       <TableCell className="p-4 border-b">{format(new Date(user.dateOfBirth!), 'dd.MM.yyyy')}</TableCell>
-       <TableCell className="p-4 border-b">{user.gender}</TableCell>
-      </TableRow>
-     ))}
-    </TableBody>
-   </Table>
-  </div>
- )
+    return (
+        <div className='container mx-auto p-6 min-h-screen '>
+            <div className='flex justify-between items-center mb-6'>
+                <h1 className='text-3xl font-bold text-gray-800'>Zoznam registrovaných na podujatie</h1>
+                <LogoutButton />
+            </div>
+            <div className='mb-4 flex space-x-4'>
+                <input
+                    type='text'
+                    placeholder='Search...'
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                    className='p-2 border border-gray-300 rounded flex-grow'
+                />
+                <button onClick={exportToPDF} className='p-2 bg-[#ff9a00] text-white rounded'>
+                    Export to PDF
+                </button>
+            </div>
+            <Table className='min-w-full bg-white shadow-md rounded-lg overflow-hidden'>
+                <TableHeader className='bg-[#ff9a00] text-white'>
+                    <TableRow>
+                        {[
+                            'eventName',
+                            'id',
+                            'fullName',
+                            'email',
+                            'city',
+                            'category',
+                            'club',
+                            'dateOfBirth',
+                            'gender',
+                        ].map((key) => (
+                            <TableCell
+                                key={key}
+                                onClick={() => handleSort(key as keyof Attendee)}
+                                className='cursor-pointer p-4 font-semibold'
+                            >
+                                {key.charAt(0).toUpperCase() + key.slice(1)}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {filteredUsers.map((user) => (
+                        <TableRow key={user.id} className='hover:bg-gray-100'>
+                            <TableCell className='p-4 border-b'>{user.eventName ?? '-'}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.id}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.fullName}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.email}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.city}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.category || '-'}</TableCell>
+                            <TableCell className='p-4 border-b'>{user.club || '-'}</TableCell>
+                            <TableCell className='p-4 border-b'>
+                                {format(new Date(user.dateOfBirth!), 'dd.MM.yyyy')}
+                            </TableCell>
+                            <TableCell className='p-4 border-b'>{user.gender}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
--- src/app/admin/users-fetch.tsx
+++ src/app/admin/users-fetch.tsx
@@ -3,19 +3,28 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { db } from '@/db/db'
-import { attendees, eventRegistrations } from '@/db/schema'
-import { eq } from 'drizzle-orm'
+import { attendees, eventRegistrations, events } from '@/db/schema'
+import { eq, getTableColumns } from 'drizzle-orm'
 
 export const fetchUsers = async () => {
-	const supabase = createServerComponentClient({ cookies })
-	const { data: { session } } = await supabase.auth.getSession()
-	if (!session) {
-		redirect('/login')
-	}
-	const users = await db
-		.select()
-		.from(attendees)
-		.leftJoin(eventRegistrations, eq(attendees.id, eventRegistrations.attendeeId))
+    const supabase = createServerComponentClient({ cookies })
+    const {
+        data: { session },
+    } = await supabase.auth.getSession()
+    if (!session) {
+        redirect('/login')
+    }
+    const users = await db
+        .select({
+            ...getTableColumns(attendees),
+            eventName: events.name,
+            eventId: events.id,
+        })
+        .from(attendees)
+        .leftJoin(eventRegistrations, eq(attendees.id, eventRegistrations.attendeeId))
+        .leftJoin(events, eq(eventRegistrations.eventId, events.id))
 
-	return users
+    return users
 }
+
+export type AttendesWithEvent = Awaited<ReturnType<typeof fetchUsers>>
